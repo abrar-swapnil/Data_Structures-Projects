21567f4f4df97664b4239867f716abb5052e70d7
SWAPNIL <swapnil@SWAPNILs-MacBook-Pro.local>
2025-10-23 19:48:33 +0600

    Find maximum value using pointer

---
+++ FindMaxUsingPointer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[20],max=0;
+    printf("Enter 5 numbers in array: ");
+
+    int *ptr = arr;
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", ptr+i);
+        
+    }
+
+    max = *ptr;
+    printf("array: ");
+    for (int i = 1; i < 5; i++)
+    {
+        if(*(ptr+i) > max) 
+        {
+            max = *(ptr+i);
+           
+        }
+    }
+    
+        printf("Maximum number is : %d\n",max);
+   
+
+    return 0;
+}
